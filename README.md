6a71ee08aa0d6858514b273785c89a3c06085ffd
masudbappy <masud.bappy90@gmail.com>
2023-09-08 22:12:44 +0600

    initial commit.

---
+++ README.md
@@ -0,0 +1,2 @@
+Creating a project
+mvn archetype:generate -DgroupId=com.masudbappy -DartifactId=microservices -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4 -DinteractiveMode=false
---
+++ customer/src/main/java/com/masudbappy/customer/CustomerApplication.java
@@ -0,0 +1,19 @@
+package com.masudbappy.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The type Customer application.
+ */
+@SpringBootApplication
+public class CustomerApplication {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerApplication.class, args);
+    }
+}
