1f35026d12d7e34940b106079651503afd2ac9e8
masudbappy <masud.bappy90@gmail.com>
2023-12-26 10:24:07 +0600

    Make customer microservice as a eureka client.

--- customer/src/main/java/com/masudbappy/customer/CustomerApplication.java
+++ customer/src/main/java/com/masudbappy/customer/CustomerApplication.java
@@ -2,11 +2,13 @@ package com.masudbappy.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * The type Customer application.
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class CustomerApplication {
     /**
      * The entry point of application.
