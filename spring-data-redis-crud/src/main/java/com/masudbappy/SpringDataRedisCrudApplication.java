25accdaf0d450722e12c9affe936c5c70397b5b1
masudbappy <masud.bappy90@gmail.com>
2024-02-01 10:20:56 +0600

    Initiate redis module with spring boot.

---
+++ spring-data-redis-crud/src/main/java/com/masudbappy/SpringDataRedisCrudApplication.java
@@ -0,0 +1,14 @@
+package com.masudbappy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The type Spring data redis crud application.
+ */
+@SpringBootApplication
+public class SpringDataRedisCrudApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataRedisCrudApplication.class);
+    }
+}
